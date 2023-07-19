8ac1826c9063bd1c0a63470357d807be8c1b3d3b
Konstantin Kharlamov <k.kharlamow@gmail.com>
2023-07-19 17:22:18 +0600

    Redundant method removed

--- src/models/BookCollection.ts
+++ src/models/BookCollection.ts
@@ -7,10 +7,6 @@ export default class BookCollection {
     this.books = books.slice();
   }
 
-  reduce = <T>(cb: (out: T, book: Book) => T, acc: T) => {
-    return this.books.reduce(cb, acc);
-  };
-
   concat = (collection: BookCollection) => {
     return new BookCollection(this.books.concat(collection.books));
   };

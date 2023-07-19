447926c88d476ff2e8c7a894158342dbd4dee22c
Konstantin Kharlamov <k.kharlamow@gmail.com>
2023-07-19 12:58:39 +0600

    Extract Function: fetchBooks.ts

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,8 @@
-import awesomeProducerData from './data/awesome_producer.json';
-import anotherProducerData from './data/another_producer.json';
-import Book from './models/Book';
+import fetchBooks from './infrastructure/fetchBooks';
 import presentation from './presentation';
 
 export default function main(asHtml = false) {
-  // @ts-ignore
-  const rawBooks = awesomeProducerData.concat(anotherProducerData);
-  const books: Book[] = [];
-
-  for (let i = 0; i < rawBooks.length; i++) {
-    let dataType = 'another';
-    let book;
-
-    if (rawBooks[i].price) {
-      dataType = 'awesome';
-    }
-
-    switch (dataType) {
-      case 'awesome':
-        book = new Book(
-          rawBooks[i].name,
-          rawBooks[i].author,
-          rawBooks[i].price / 1000000
-        );
-        break;
-      case 'another':
-        book = new Book(
-          // @ts-ignore
-          rawBooks[i].title,
-          rawBooks[i].author,
-          // @ts-ignore
-          rawBooks[i].amount - rawBooks[i].amount / 100 * rawBooks[i].discount
-        );
-        break;
-    }
-
-    books.push(book);
-  }
-
+  const books = fetchBooks();
   let output = '';
 
   books.forEach((book) => {
---
+++ src/infrastructure/fetchBooks.ts
@@ -0,0 +1,41 @@
+import awesomeProducerData from '../data/awesome_producer.json';
+import anotherProducerData from '../data/another_producer.json';
+import Book from '../models/Book';
+
+export default function fetchBooks() {
+  // @ts-ignore
+  const rawBooks = awesomeProducerData.concat(anotherProducerData);
+  const books: Book[] = [];
+
+  for (let i = 0; i < rawBooks.length; i++) {
+    let dataType = 'another';
+    let book;
+
+    if (rawBooks[i].price) {
+      dataType = 'awesome';
+    }
+
+    switch (dataType) {
+      case 'awesome':
+        book = new Book(
+          rawBooks[i].name,
+          rawBooks[i].author,
+          rawBooks[i].price / 1000000
+        );
+        break;
+      case 'another':
+        book = new Book(
+          // @ts-ignore
+          rawBooks[i].title,
+          rawBooks[i].author,
+          // @ts-ignore
+          rawBooks[i].amount - rawBooks[i].amount / 100 * rawBooks[i].discount
+        );
+        break;
+    }
+
+    books.push(book);
+  }
+
+  return books;
+}

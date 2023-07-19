365bdec55b00e50194c79afdaefea25b12a7e4dd
Konstantin Kharlamov <k.kharlamow@gmail.com>
2023-07-19 15:40:12 +0600

    Replace Conditional with Polymorphism, Part Three -- composite pattern finalized, dependency inversion applied

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
 import BookRepositoryComposite from './infrastructure/BookRepositoryComposite';
+import AwesomeBookRepository from './infrastructure/AwesomeBookRepository';
+import AnotherBookRepository from './infrastructure/AnotherBookRepository';
 import presentation from './presentation';
 
 export default function main(asHtml = false) {
-  return new BookRepositoryComposite().all().reduce((output, book) => {
+  const bookRepository = new BookRepositoryComposite();
+
+  bookRepository.addRepository(new AwesomeBookRepository());
+  bookRepository.addRepository(new AnotherBookRepository());
+
+  return bookRepository.all().reduce((output, book) => {
     output += presentation(asHtml, book);
 
     return output;
   }, '');
-}
+};
--- src/infrastructure/BookRepositoryComposite.ts
+++ src/infrastructure/BookRepositoryComposite.ts
@@ -1,11 +1,19 @@
 import BookRepository from '../models/BookRepository';
-import AwesomeBookRepository from './AwesomeBookRepository';
-import AnotherBookRepository from './AnotherBookRepository';
 
 export default class BookRepositoryComposite implements BookRepository {
+  private readonly repositories = new Set<BookRepository>();
+
+  addRepository = (bookRepository: BookRepository) => {
+    this.repositories.add(bookRepository);
+  };
+
   all = () => {
-    return new AwesomeBookRepository()
-      .all()
-      .concat(new AnotherBookRepository().all());
+    let result = [];
+
+    this.repositories.forEach((bookRepository) => {
+      result = result.concat(bookRepository.all());
+    });
+
+    return result;
   }
 }

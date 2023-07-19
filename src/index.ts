22ad35707dcd73ac3005484284b7091a54794e6a
Konstantin Kharlamov <k.kharlamow@gmail.com>
2023-07-19 13:00:42 +0600

    Inline Variable: books is redundant

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,9 @@ import fetchBooks from './infrastructure/fetchBooks';
 import presentation from './presentation';
 
 export default function main(asHtml = false) {
-  const books = fetchBooks();
   let output = '';
 
-  books.forEach((book) => {
+  fetchBooks().forEach((book) => {
     output += presentation(asHtml, book);
   });
 
